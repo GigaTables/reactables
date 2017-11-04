59d04bbd3dbd86cd25d4428262b476b4e45175d1
zeusakm <zeusakm@gmail.com>
2017-11-04 09:29:44 +0300

    *	#71. test global css through attrs className + readme

--- main.js
+++ main.js
@@ -5,154 +5,163 @@ import Header from './src/components/Header.js'
 import mainStyles from './main.css'
 
 let editor = {
-  ajax: 'http://gigatables.loc/editor.php',
-  ajaxFiles: 'http://gigatables.loc/uploadFiles.php',
-  struct: {
-    buttons: ['top', 'bottom'] // buttons
-  },
-  fields: [
-    {
-      label: "ID",
-      name: "id",
-      type: 'hidden'
+    ajax: 'http://gigatables.loc/editor.php',
+    ajaxFiles: 'http://gigatables.loc/uploadFiles.php',
+    struct: {
+        buttons: ['top', 'bottom'] // buttons
     },
-    {// an example of using select - automatically selected if matches with data in table column
-      label: "Types:",
-      name: "types[]",
-      values: [// if select,checkbox,radio etc types - need this pre-set structure of values
-        {'key1': 'val1'},
-        {'key2': 'val2'}
-      ],
-      type: 'checkbox', // select,checkbox,radio
+    fields: [
+        {
+            label: "ID",
+            name: "id",
+            type: 'hidden'
+        },
+        {// an example of using select - automatically selected if matches with data in table column
+            label: "Types:",
+            name: "types[]",
+            values: [// if select,checkbox,radio etc types - need this pre-set structure of values
+                {'key1': 'val1'},
+                {'key2': 'val2'}
+            ],
+            type: 'checkbox', // select,checkbox,radio
 //              attrs: [
 //                {'multiple':true}
 //              ]
-    },
-    {
-      label: "Article title:",
-      name: "title",
-      type: 'text', // default, other: password, file, select, multiselect etc
-      attrs: [
-        {'pattern': '^[A-Za-z0-9_]+$'},
-          {'className': 'titleField'}
-      ]
-    },
-    {
-      label: "Description:",
-      name: "desc",
-      type: 'textarea'
-    },
-    {
-      label: "Date Time:",
-      name: "date",
-      type: 'date'
-    },
-    {
-      label: "Image:",
-      name: "image",
-      type: 'file'
-    },
-  ]
+        },
+        {
+            label: "Article title:",
+            name: "title",
+            type: 'text', // default, other: password, file, select, multiselect etc
+            attrs: [
+                {'pattern': '^[A-Za-z0-9_]+$'},
+                {'className': 'titleField'}
+            ]
+        },
+        {
+            label: "Description:",
+            name: "desc",
+            type: 'textarea',
+            attrs: [
+                {'className': 'descriptionField'}
+            ]
+        },
+        {
+            label: "Date Time:",
+            name: "date",
+            type: 'date'
+        },
+        {
+            label: "Image:",
+            name: "image",
+            type: 'file'
+        },
+    ]
 };
 
 let settings = {
- struct: {
-   search: ['top', 'bottom'],
-   rowsSelector: ['desc', 'top', 'bottom'],
-   pagination: ['bottom'],
-   fixedHeader: false, // default false
-   editableCells: true, // default false
- },
- lang: 'en', // english default
- perPageRows: [25, 50, 100, 200],
- defaultPerPage: 100,
- ajax: 'http://gigatables.loc/gigatables.php',
- // ajaxAutoloadData: true, // default false
- // ajaxAutoloadPeriod: 8, // sec
- requestType: 'GET',
- columns: [
-   {// include all defaults
-     data: "id",
-     sortable: true, // true by defualt
-     visible: true, // true by defualt
-     searchable: true, // true by defualt
-     discreteSearch: true, // false by default
-     discreteSearchValue: function (title) {
-       return 'Search by field - ' + title;
-     }
-   },
-   {
-     data: "title",
-     cISearch: true // default false
-   },
-   {
-     data: "desc",
-     sortable: false,
-     discreteSearch: true,
-     discreteCISearch: true // default false
-   },
-   {
-     data: "date",
-     searchable: false
-   },
-   {
-     data: "info"
-   },
-   {data:"field1"},
-   {data:"field2"},
-   {data:"field3", visible: false}
- ],
- columnOpts: [
-   {
-     render: (data, row, type) => (
-       <div>
-         <form method="post" action="">
-          <input type="hidden" name="action" value="forms" />
-          <input type="hidden" name="id" value={row.id} />
-          <div>{data}</div>
-        </form>
-      </div>),
-     target: 'title' // provide data column index to match opts
-   },
-   {
-     render: (data, row, type) => (
-        <div>
-          <div>{data}</div>
-          <div>{(row.id % 2 === 0) ? '**' : '*'}</div>
-        </div>
-       ),
-     target: 'date'
-   }
- ],
- tableOpts: {
-   buttons: [
-     {extended: "editor_create", editor: editor, triggerAfter: (function () {
-         console.log('after create');
-       }), triggerBefore: (function () {
-         console.log('before create');
-       })},
-     {extended: "editor_edit", editor: editor, triggerBefore: (function () {
-       console.log('before edit');
-     })},
-     {extended: "editor_remove", editor: editor, triggerAfter: (function () {
-         console.log('after del');
-       })}
-   ],
-   buttonsPosition: ['top', 'bottom'],
-   theme: 'std'
- }
+    struct: {
+        search: ['top', 'bottom'],
+        rowsSelector: ['desc', 'top', 'bottom'],
+        pagination: ['bottom'],
+        fixedHeader: false, // default false
+        editableCells: true, // default false
+    },
+    lang: 'en', // english default
+    perPageRows: [25, 50, 100, 200],
+    defaultPerPage: 100,
+    ajax: 'http://gigatables.loc/gigatables.php',
+    // ajaxAutoloadData: true, // default false
+    // ajaxAutoloadPeriod: 8, // sec
+    requestType: 'GET',
+    columns: [
+        {// include all defaults
+            data: "id",
+            sortable: true, // true by defualt
+            visible: true, // true by defualt
+            searchable: true, // true by defualt
+            discreteSearch: true, // false by default
+            discreteSearchValue: function (title) {
+                return 'Search by field - ' + title;
+            }
+        },
+        {
+            data: "title",
+            cISearch: true // default false
+        },
+        {
+            data: "desc",
+            sortable: false,
+            discreteSearch: true,
+            discreteCISearch: true // default false
+        },
+        {
+            data: "date",
+            searchable: false
+        },
+        {
+            data: "info"
+        },
+        {data: "field1"},
+        {data: "field2"},
+        {data: "field3", visible: false}
+    ],
+    columnOpts: [
+        {
+            render: (data, row, type) => (
+                <div>
+                    <form method="post" action="">
+                        <input type="hidden" name="action" value="forms"/>
+                        <input type="hidden" name="id" value={row.id}/>
+                        <div>{data}</div>
+                    </form>
+                </div>),
+            target: 'title' // provide data column index to match opts
+        },
+        {
+            render: (data, row, type) => (
+                <div>
+                    <div>{data}</div>
+                    <div>{(row.id % 2 === 0) ? '**' : '*'}</div>
+                </div>
+            ),
+            target: 'date'
+        }
+    ],
+    tableOpts: {
+        buttons: [
+            {
+                extended: "editor_create", editor: editor, triggerAfter: (function () {
+                console.log('after create');
+            }), triggerBefore: (function () {
+                console.log('before create');
+            })
+            },
+            {
+                extended: "editor_edit", editor: editor, triggerBefore: (function () {
+                console.log('before edit');
+            })
+            },
+            {
+                extended: "editor_remove", editor: editor, triggerAfter: (function () {
+                console.log('after del');
+            })
+            }
+        ],
+        buttonsPosition: ['top', 'bottom'],
+        theme: 'std'
+    }
 };
 
 ReactDOM.render(
-  <Reactables editor={editor} settings={settings}>
-    <Header data="id">ID</Header>
-    <Header data="title">Name</Header>
-    <Header data="desc">Description</Header>
-    <Header data="date">Date</Header>
-    <Header data="info">Info</Header>
-    <Header data="field2">Field123 but data from field2</Header>
-    <Header data="field1">Field1</Header>
-    <Header data="field3">Field3 invisible</Header>
-    <Header>Field3 invisible</Header>
-  </Reactables>,
-  document.getElementById('app'));
+    <Reactables editor={editor} settings={settings}>
+        <Header data="id">ID</Header>
+        <Header data="title">Name</Header>
+        <Header data="desc">Description</Header>
+        <Header data="date">Date</Header>
+        <Header data="info">Info</Header>
+        <Header data="field2">Field123 but data from field2</Header>
+        <Header data="field1">Field1</Header>
+        <Header data="field3">Field3 invisible</Header>
+        <Header>Field3 invisible</Header>
+    </Reactables>,
+    document.getElementById('app'));

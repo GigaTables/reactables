3c0a95c2dc76efe71bca569af4d5dae0f64326c0
akushman <zeusakm@gmail.com>
2018-05-13 08:58:53 +0300

    #105. Add more caverage on PagesSelector component

--- __tests__/components/PagesSelector.test.js
+++ __tests__/components/PagesSelector.test.js
@@ -1,33 +1,34 @@
-import React from 'react';
-import renderer from 'react-test-renderer';
+import React from 'react'
+import renderer from 'react-test-renderer'
 import PagesSelector from '../../src/components/tools/PagesSelector.js'
-import {shallow, configure} from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
-configure({adapter: new Adapter()});
+import { shallow, configure } from 'enzyme'
+import Adapter from 'enzyme-adapter-react-16'
+
+configure({ adapter: new Adapter() })
 
 it('renders PagesSelector correctly', () => {
     const tree = renderer.create(
         <PagesSelector
             lang="en"
             defaultPerPage="100"
-            perPageRows={[25, 50, 100, 200]}
+            perPageRows={[ 25, 50, 100, 200 ]}
             page="1"
-            perPage={50}
-            updatePerPage={(e) => {}}/>
-    ).toJSON();
-    expect(tree).toMatchSnapshot();
-
+            perPage={50}/>
+    ).toJSON()
+    expect(tree).toMatchSnapshot()
+    
     const obj = shallow(
         <PagesSelector
             lang="en"
             defaultPerPage="100"
-            perPageRows={[25, 50, 100, 200]}
+            perPageRows={[ 25, 50, 100, 200 ]}
             page="1"
-            perPage={50}/>
-    );
-
+            perPage={50}
+            updatePerPage={(e) => {}}/>
+    )
     obj.instance().shouldComponentUpdate({
-        perPageRows: [25, 50, 100, 200],
+        perPageRows: [ 25, 50, 100, 200 ],
         perPage: 100
-    });
-});
+    })
+    obj.find('select').simulate('change')
+})
